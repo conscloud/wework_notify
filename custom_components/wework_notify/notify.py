6dddaf48450f23a29878e4ebe2199af2de13f84e
conscloud <conscloud@gmail.com>
2022-10-23 20:36:30 +0800

    Update notify.py

--- custom_components/wework_notify/notify.py
+++ custom_components/wework_notify/notify.py
@@ -114,8 +114,6 @@ class WeWorkNotificationService(BaseNotificationService):
                 content += f"{title}\n{DIVIDER}\n"
             content += message
             msg = {"content": content}
-        elif msgtype == "markdown":
-            msg = {"content": message}
         elif msgtype == "textcard":
             msg = {"title": title, "description": message, "url": url}
         elif msgtype == "news":
